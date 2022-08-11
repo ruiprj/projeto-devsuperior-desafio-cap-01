3e1598e203d922ee490c69299f4b394f1f45ee5e
Rui Palmeira Reis Junior <ruiprj@gmail.com>
2022-08-11 12:13:17 -0400

    Navbar styling and hamburguer menu

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,24 +1,41 @@
-const Navbar = () => {
-    return (
-        <nav className="bg-primary">
-            <div>
-                <a href="link">
-                    <h4>Carros Top</h4>
-                </a>
+import './styles.css';
+import 'bootstrap/js/src/collapse.js';
 
-                <div>
-                    <ul>
-                        <li>
-                            <a href="link">HOME</a>
-                        </li>
-                        <li>
-                            <a href="link">CATÁLOGO</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+const Navbar = () => {
+  return (
+    <nav className="navbar  navbar-expand-md  bg-primary  main-nav">
+      <div className="container-fluid">
+        <a href="link" className="nav-logo-text">
+          <h4>Carros Top</h4>
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#carrostop-navbar"
+          aria-controls="carrostop-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse  navbar-collapse  main-menu-items" id="carrostop-navbar">
+          <ul className="navbar-nav  offset-md-2  main-menu">
+            <li>
+              <a href="link" className="active">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="link">Catálogo</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
