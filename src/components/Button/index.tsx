e36257f571460f303b5e4c36b0737836a878eab1
Rui Palmeira Reis Junior <ruiprj@gmail.com>
2022-08-15 12:55:15 -0400

    Innitial Home cards configuration

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const Button = () => {
+    return (
+        <button className="btn  btn-custom">
+            <h6>VER CATÁLOGO</h6>
+        </button>
+    );
+}
+
+export default Button;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import MainImage from 'assets/images/main-image-yellow-car.png';
+import Button from 'components/Button';
 import Navbar from 'components/Navbar';
 import './styles.css';
 
@@ -8,20 +9,27 @@ const Home = () => {
       <Navbar />
 
       <div className="home-container">
-        <div className="home-card">
+        <div className="home-card-image">
           <div className="home-image-container">
-            <img src={MainImage} alt="Imagem principal" />
+            <div className="home-image-container-content">
+              <h1>O carro perfeito para você</h1>
+              <p>Conheça nossos carros e dê mais um passo na realização do seu sonho</p>
+            </div>
 
-            <h1>O carro perfeito para você</h1>
-            <p>Conheça nossos carros e dê mais um passo na realização do seu sonho</p>
+            <div>
+              <img src={MainImage} alt="Imagem principal" />
+            </div>
           </div>
+        </div>
 
+        <div className="home-card-content">
           <div className="home-content-container">
-            <h1>botão aqui!</h1>
+            <div>
+              <Button />
+            </div>
 
             <p>Comece agora a navegar</p>
           </div>
-
         </div>
       </div>
     </>
